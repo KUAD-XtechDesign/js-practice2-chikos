dcde1b27ca56e0f6b89aab0b92d25b54a7e583ae
chikos <68629026+chikos@users.noreply.github.com>
2020-10-29 23:47:09 +0900

    k

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,4 +1,30 @@
 $(function(){
+    let hako=0;
+
+    $("button01").on("click",function(){
+        for(let i=0;i<13;i++){
+            hako=hako+5;
+        }
+    $("#content").text(hako);
+})
+
+$("#button02").on("click",function(){
+    for(let i=0;i<12;i++){
+    hako=hako+1;
+    }
+    $("#content").text(hako);
+})
+$("button03").on("click",function(){
+    setInterval(function(){
+        hako=hako+2;
+        $("#content").text(hako);
+    },1000)
+})
+
+$("button04").on("click",function(){
+    hako=0;
+    $("#content").text(hako);
+})
 
 
   
