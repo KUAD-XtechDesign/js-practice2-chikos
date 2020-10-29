b2d8b426b69a14e67423e392fc1e09c8ff84a21b
chikos <68629026+chikos@users.noreply.github.com>
2020-10-30 00:36:19 +0900

    w

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -31,7 +31,7 @@ $(function(){
       samTwo(num1,num2)
   })
   $("#button05").on("click",function(){
-      samTwo(txt1,txt2)
-      samTwo(num1,num2)
+      samTwo(txt1,txt2,num1,num2)
+     
   })
 })
