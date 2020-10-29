6613bdbe1738e51d05c796cda5cacb0f8191e912
chikos <68629026+chikos@users.noreply.github.com>
2020-10-29 23:59:52 +0900

    aa

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,32 @@
 $(function(){
 
+    let hako=0;
 
+    function checkbox(){
+        if(hako>5){
+            $("#content").text(hako+ "つです〜。箱はいっぱいいっぱいお腹つらい。ちょっとお手洗い行きます。");
+            hako=0;
+        }else if(hako<=0){
+            hako=0;
+            $("#content").text(hako+"つです〜。じ・つ・は・入ってないんですよ〜。");
 
+        }else{
+            $("#content").text(hako+"つですねえ。まだ食べてもいいですか?");
+
+        }
+    }
+
+    $("#button01").on("click",function(){
+        checkBox()
+    })
+
+    $("#button02").on("click",function(){
+        hako=hako+1;
+        checkBox()
+    })
+
+    $("#button03").on("click",function(){
+        hakoa=hako-1;
+        checkBox()
+    })
 })
